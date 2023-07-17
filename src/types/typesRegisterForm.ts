8e647b234be8307cc19ae2cf17f6834cd5e8ae30
gabriel <gabriel@pop-os.localdomain>
2023-07-17 14:55:30 -0300

    :up: change structure

---
+++ src/types/typesRegisterForm.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const schemaFormValidation = z.object({
+  name: z.string().min(3, 'Por favor digite um nome válido'),
+  email: z.string().email('Por favor digite um email válido.'),
+  birth: z.coerce.date().refine((date) => date < new Date(), {
+    message: 'Data invalida, por favor selecione uma data válida',
+  }),
+  tel: z
+    .string()
+    .min(15, 'Informe um numero de telefone válido')
+    .max(15, 'Informe um numero de telefone válido')
+    .nonempty('Informe um numero de telefone'),
+  gender: z.string().nonempty('Por favor selecione seu gênero'),
+})
+
+export type TypeFormRegisterOngs = z.infer<typeof schemaFormValidation>
